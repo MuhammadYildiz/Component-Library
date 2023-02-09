a03317baf25dd76c15c55d15cb9e8ac61c4fe47a
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-02-09 12:09:06 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,14 +21,14 @@ export default function About() {
               </div>
             </div>
             <div className="flex flex-wrap -mx-4">
-              <div className="px-4 relative w-full lg:w-4/12 w-full md:w-6/12">
-                <div className="mb-12 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 rounded-lg">
+              <div className="px-4 relative  lg:w-4/12 w-full md:w-6/12">
+                <div className=" relative flex flex-col min-w-0 break-words bg-white w-full mb-6 rounded-lg">
                   <a href="#">
                     <img alt="..." className="shadow-lg rounded-lg w-full flex-shrink-0" src="https://demos.creative-tim.com/notus-pro-react/static/media/castle-1.180e8ed5.jpg" />
                     <h3 className="text-2xl font-bold leading-tight ml-1 mt-6">That’s One Way To Ditch Your Castle</h3>
                   </a>
                   <p className="mt-3 leading-relaxed text-blueGray-500 ml-1">Finding temporary housing should be as easy as renting an Airbnb. That’s the idea behind Portofino, which raised $65 million to expand its pet sitting businesses. This come as the right move for the investment while the planet is moving on work from home...</p>
-                  <div className="mt-6 flex items-center">
+                  <div className="mt-6 flex items-center flex-col">
                     <img alt="..." src="https://demos.creative-tim.com/notus-pro-react/static/media/team-4.639c2559.jpg" className="rounded-full mr-3 max-w-40-px" />
                     <div className="flex-1">
                       <div className="justify-between items-center">
@@ -38,14 +38,14 @@ export default function About() {
                   </div>
                 </div>
               </div>
-              <div className="px-4 relative w-full lg:w-4/12 w-full md:w-6/12">
-                <div className="mb-12 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 rounded-lg">
+              <div className="px-4 relative  lg:w-4/12 w-full md:w-6/12">
+                <div className=" relative flex flex-col min-w-0 break-words bg-white w-full mb-6 rounded-lg">
                   <a href="#">
                     <img alt="..." className="shadow-lg rounded-lg w-full flex-shrink-0" src="https://demos.creative-tim.com/notus-pro-react/static/media/pawel-nolbert.6876e1e9.jpg" />
                     <h3 className="text-2xl font-bold leading-tight ml-1 mt-6">Society of yesterday, today and tomorrow</h3>
                   </a>
                   <p className="mt-3 leading-relaxed text-blueGray-500 ml-1">Society has put up so many boundaries, so many limitations on what’s right and wrong that it’s almost nice. Let me tell you the base rule of living on the streets, you need to respect them.</p>
-                  <div className="mt-6 flex items-center">
+                  <div className="mt-6 flex items-center flex-col">
                     <img alt="..." src="https://demos.creative-tim.com/notus-pro-react/static/media/team-1.26905a67.jpg" className="rounded-full mr-3 max-w-40-px" />
                     <div className="flex-1">
                       <div className="justify-between items-center">
@@ -55,14 +55,14 @@ export default function About() {
                   </div>
                 </div>
               </div>
-              <div className="px-4 relative w-full lg:w-4/12 w-full md:w-6/12">
-                <div className="mb-12 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 rounded-lg">
+              <div className="px-4 relative  lg:w-4/12 w-full md:w-6/12">
+                <div className=" relative flex flex-col min-w-0 break-words bg-white w-full mb-6 rounded-lg">
                   <a href="#">
                     <img alt="..." className="shadow-lg rounded-lg w-full flex-shrink-0" src="https://demos.creative-tim.com/notus-pro-react/static/media/twk-tt.cff2e507.jpg" />
                     <h3 className="text-2xl font-bold leading-tight ml-1 mt-6">New machine learning models for IFTTT</h3>
                   </a>
                   <p className="mt-3 leading-relaxed text-blueGray-500 ml-1">Let me tell you the base rule of living on the streets, you need to respect them. If you’ve ever wanted to train a machine learning model and integrate it with IFTTT. You have a street life, you need to live it like so.</p>
-                  <div className="mt-6 flex items-center">
+                  <div className="mt-6 flex flex-col  items-center">
                     <img alt="..." src="https://demos.creative-tim.com/notus-pro-react/static/media/michael.660d3e04.jpg" className="rounded-full mr-3 max-w-40-px" />
                     <div className="flex-1">
                       <div className="justify-between items-center">
